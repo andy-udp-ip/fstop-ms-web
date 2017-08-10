50cda80225638968dad4f9dd7fca4516cf0bb6fc
andy <andy.udp.ip@gmail.com>
2017-08-10 20:01:32 +0800

    first commit

---
+++ src/main/java/tw/com/fstop/FstopMsWebApplication.java
@@ -0,0 +1,12 @@
+package tw.com.fstop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FstopMsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FstopMsWebApplication.class, args);
+	}
+}
