f9c38277045fec3e70887b313fdc1bca39f27895
andy <andy.udp.ip@gmail.com>
2018-01-30 21:59:06 +0800

    add logback

--- src/main/java/tw/com/fstop/app/controller/DemoController.java
+++ src/main/java/tw/com/fstop/app/controller/DemoController.java
@@ -16,15 +16,21 @@ package tw.com.fstop.app.controller;
 import java.util.Date;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController
 {
+    private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+    
     @GetMapping("/")
     public String demo(Map<String, Object> model)
     {
+        log.debug("demo");
+        
         model.put("time", new Date());
         model.put("message", "demo demo");
         return "demo";
