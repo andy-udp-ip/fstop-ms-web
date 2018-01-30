6915fe54778ba8b71ee05c0031f926032db61b12
andy <andy.udp.ip@gmail.com>
2018-01-30 17:29:55 +0800

    add testing case

--- src/test/java/tw/com/fstop/FstopMsWebApplicationTests.java
+++ src/test/java/tw/com/fstop/FstopMsWebApplicationTests.java
@@ -2,15 +2,29 @@ package tw.com.fstop;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class FstopMsWebApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class FstopMsWebApplicationTests
+{
+    @Autowired
+    private TestRestTemplate restTemplate;
+    
+    @Test
+    public void accessJspPageAndGetCorrectResopnse()
+    {
+        ResponseEntity<String> entity = this.restTemplate.getForEntity("/", String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody()).contains("spring url");
+    }
 
 }
